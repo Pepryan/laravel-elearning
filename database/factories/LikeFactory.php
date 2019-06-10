2c6a5f1d5d38672732830e2083a292a9cc1a69d9
Pepryan <febryanramadhan@gmail.com>
2019-06-10 17:21:21 +0700

    Factory and Seed

--- database/factories/LikeFactory.php
+++ database/factories/LikeFactory.php
@@ -2,11 +2,13 @@
 
 /* @var $factory \Illuminate\Database\Eloquent\Factory */
 
-use App\Model\Like;
 use Faker\Generator as Faker;
+use App\User;
 
-$factory->define(Like::class, function (Faker $faker) {
+$factory->define(App\Model\Like::class, function (Faker $faker) {
     return [
-        //
+        'user_id' => function () {
+            return \App\User::all()->random();
+        }
     ];
 });
--- database/factories/QuestionFactory.php
+++ database/factories/QuestionFactory.php
@@ -2,11 +2,20 @@
 
 /* @var $factory \Illuminate\Database\Eloquent\Factory */
 
-use App\Model\Question;
+use App\Model\Category;
 use Faker\Generator as Faker;
 
-$factory->define(Question::class, function (Faker $faker) {
+$factory->define(App\Model\Question::class, function (Faker $faker) {
+    $title = $faker->sentence;
     return [
-        //
+        'title' => $title,
+        'slug' => str_slug($title),
+        'body' => $faker->text,
+        'category_id' => function () {
+            return \App\Model\Category::all()->random();
+        },
+        'user_id' => function () {
+            return \App\User::all()->random();
+        },
     ];
 });
